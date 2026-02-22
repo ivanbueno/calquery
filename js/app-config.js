7f9ab7ebb03fdf86da64ce841c34d8e66de9a3ab
ivanbueno-jcc <ivanbueno-jcc@users.noreply.github.com>
2026-02-22 10:30:50 +0000

    Deploy web from ivanbueno/civra@bf6bc2584b7562fc6dc99b558744c72d8f14fd2b ivanbueno/civra@bf6bc2584b7562fc6dc99b558744c72d8f14fd2b

--- js/app-config.js
+++ js/app-config.js
@@ -1,5 +1,5 @@
 window.RAG_CHATBOT_CONFIG = {
-  "generated_at": "2026-02-22T00:30:55.994731+00:00",
+  "generated_at": "2026-02-22T10:29:55.021587+00:00",
   "orchestrator": {
     "function_name": "rag-chatbot-orchestrator-router",
     "function_url": "https://lsldyl7tiq4rd42dvukd4mrgtm0jcvmz.lambda-url.us-east-1.on.aws/"
